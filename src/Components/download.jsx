fa8f07ca7d6a8cf91346853e2f10e4b47cf02008
Abdelrahman-Afia <abdelrahmanafia20@gmail.com>
2025-11-24 14:10:06 +0200

    Download Button Activated

--- src/Components/download.jsx
+++ src/Components/download.jsx
@@ -4,10 +4,16 @@ import img1 from "../Assets/Download Icon.png";
 
 const Download = () => {
   return (
-    <button className="download-btn">
-      <img src={img1} alt="download-icon" className="download-icon" />
-      <span className="download-style">Download CV</span>
-    </button>
+    <a
+      href="https://drive.google.com/file/d/1pfkgoSSaHGt448Lzk8p_aLh_oRn693yw/view?usp=drive_link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button className="download-btn">
+        <img src={img1} alt="download-icon" className="download-icon" />
+        <span className="download-style">Download CV</span>
+      </button>
+    </a>
   );
 };
 
